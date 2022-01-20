8779025d03243e803722bbb49686c215c8d4057e
Adriana Barros <adri_gfb@hotmail.com>
2022-01-20 16:08:13 +0000

    Correções e implementação de novas features

---
+++ wp-content/themes/apikiblog/app.js
@@ -0,0 +1,15 @@
+let requestPosts = new XMLHttpRequest();
+requestPosts.open('GET', 'https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518');
+requestPosts.onload = () => {
+  if (requestPosts.status >= 200 && requestPosts.status < 400) {
+    let data = JSON.parse(requestPosts.responseText);
+  } else {
+    console.log("We connected to the server, but it returned an error.");
+  }
+};
+
+requestPosts.onerror = () => {
+  console.log("Connection error");
+};
+
+requestPosts.send();
--- wp-content/themes/apikiblog/functions.php
+++ wp-content/themes/apikiblog/functions.php
@@ -1,7 +1,10 @@
 <?php 
 
     function apiki_files() {
-        wp_enqueue_style('apiki_styles', get_stylesheet_uri());
+
+        wp_enqueue_style('style', get_stylesheet_uri());
+        wp_enqueue_script('app_js', get_template_directory_uri() . '/app.js', true);
+
     }
 
     add_action('wp_enqueue_scripts', 'apiki_files');
